2d58614e5ed32290aa0e446accb423d8a54a9588
Yong Zeng <dustinzeng0608@gmail.com>
2018-04-27 21:15:05 -0400

    Initial Commit

---
+++ Algorithm_OC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Algorithm_OC
+//
+//  Created by Yong Zeng on 4/27/18.
+//  Copyright © 2018 Yong Zeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
